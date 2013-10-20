b5645a3d1094f9bb074fe67bc9f19bf59e43778c
vsmasster <krasnal1995@gmail.com>
2013-10-20 20:47:19 +0200

    uaktualniony main
    vector<double> jest w Plik.Trans, ktorej funkcji mam go przekazac?

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 #include<vector>
 #include<cmath>
 #include "compute.h"
+#include "wav.h"
 using namespace std;
 
 int main()
 {
+	wav Plik("test2.wav");
     vector<double> ssin;
     for(int i=0;i<1<<20;i++)
         ssin.push_back(sin((double)i/128));
